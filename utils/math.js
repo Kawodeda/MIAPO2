29f6af6f8c610101b2b4ab6db73eceb1718735cb
Kawodeda <konserva023@gmail.com>
2023-05-02 10:43:09 +0700

    add nearlyEquals

--- utils/math.js
+++ utils/math.js
@@ -1,3 +1,9 @@
 export function clamp(value, min, max) {
     return Math.min(Math.max(value, min), max);
 }
+
+export function nearlyEquals(a, b, epsilon = 0.00001) {
+    const diff = Math.abs(a - b);
+
+    return diff < epsilon;
+}
